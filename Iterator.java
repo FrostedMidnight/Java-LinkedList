36e8bc08ca75abc31ac6bb4ce2c4900c6f190117
FrostedMidnight <frostedmidnight4@proton.me>
2025-12-17 19:07:46 -0500

    Create Iterator and Consumer classes

---
+++ Iterator.java
@@ -0,0 +1,18 @@
+//https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/Iterator.html
+
+//An iterator over a collection.
+public interface Iterator<E> {
+    //Returns true if the iteration has more elements.
+    boolean hasNext();
+
+    //Returns the next element in the iteration.
+    E next();
+
+    //Removes from the underlying collection the last element returned by this iterator (optional operation).
+    //This method can be called only once per call to next().
+    default void remove()
+        throws UnsupportedOperationException, IllegalStateException {
+        //The default implementation throws an instance of UnsupportedOperationException and performs no other action.
+        throw new UnsupportedOperationException();
+    }
+}
