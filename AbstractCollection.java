8d40d048b231466fce6484b055495bc5acafff71
FrostedMidnight <frostedmidnight4@proton.me>
2025-12-17 19:24:57 -0500

    Create Collection interface and AbstractCollection abstract class

---
+++ AbstractCollection.java
@@ -0,0 +1,4 @@
+//https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/AbstractCollection.html
+
+//This class provides a skeletal implementation of the Collection interface, to minimize the effort required to implement this interface.
+public abstract class AbstractCollection<E> implements Collection<E> {}
---
+++ Collection.java
@@ -0,0 +1,7 @@
+//https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/Collection.html
+
+//The root interface in the collection hierarchy.
+//A collection represents a group of objects, known as its elements.
+//Some collections allow duplicate elements and others do not.
+//Some are ordered, and others are unordered.
+public interface Collection<E> extends Iterable<E> {}
