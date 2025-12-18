d58890ce3630aacaae301186ca40407e2e196279
FrostedMidnight <frostedmidnight4@proton.me>
2025-12-17 21:35:10 -0500

    Create Deque interface

--- LinkedList.java
+++ LinkedList.java
@@ -1,11 +1,11 @@
-//https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/LinkedList.html
+// https://docs.oracle.com/en/java/javase/25/docs/api/java.base/java/util/LinkedList.html
 
-//Doubly-linked list implementation of the List and Deque interfaces.
-//Implements all optional list operations, and permits all elements (including null).
+// Doubly-linked list implementation of the List and Deque interfaces.
+// Implements all optional list operations, and permits all elements (including null).
 
 import java.util.NoSuchElementException;
 
-public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
+public class LinkedList<E> extends AbstractCollection<E> implements Deque<E> {
 
     private static class Node<E> {
 
@@ -20,12 +20,80 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
 
     private Node<E> head, tail;
 
-    //Constructs an empty list.
+    // Constructs an empty list.
     public LinkedList() {
         this.head = this.tail = null;
     }
 
-    //Returns the number of elements in this list.
+    // Returns the first element in this list.
+    @Override
+    public E getFirst() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return this.head.val;
+        }
+    }
+
+    // Returns the last element in this list.
+    @Override
+    public E getLast() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return this.tail.val;
+        }
+    }
+
+    // Removes and returns the first element from this list.
+    public E removeFirst() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            E returnElement = this.head.val;
+            this.head = this.head.next;
+            this.head.prev = null;
+            return returnElement;
+        }
+    }
+
+    // Removes and returns the last element from this list.
+    public E removeLast() throws NoSuchElementException {
+        if (this.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            E returnElement = this.tail.val;
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+            return returnElement;
+        }
+    }
+
+    // Inserts the specified element at the beginning of this list.
+    public void addFirst(E e) {
+        if (this.isEmpty()) {
+            this.head = new Node<>(e);
+            this.tail = this.head;
+        } else {
+            this.head.prev = new Node<>(e);
+            this.head.prev.next = this.head;
+            this.head = this.head.prev;
+        }
+    }
+
+    // Appends the specified element to the end of this list.
+    public void addLast(E e) {
+        if (this.isEmpty()) {
+            this.head = new Node<>(e);
+            this.tail = this.head;
+        } else {
+            this.tail.next = new Node<>(e);
+            this.tail.next.prev = this.tail;
+            this.tail = this.tail.next;
+        }
+    }
+
+    // Returns the number of elements in this list.
     @Override
     public int size() {
         int count = 0;
@@ -39,8 +107,8 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         return count;
     }
 
-    //Appends the specified element to the end of this list.
-    //This method is equivalent to addLast(E).
+    // Appends the specified element to the end of this list.
+    // This method is equivalent to addLast(E).
     @Override
     public boolean add(E e) {
         if (this.isEmpty()) {
@@ -55,7 +123,7 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         return true;
     }
 
-    //Retrieves, but does not remove, the head (first element) of this list.
+    // Retrieves, but does not remove, the head (first element) of this list.
     @Override
     public E peek() {
         if (this.isEmpty()) {
@@ -65,7 +133,7 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         }
     }
 
-    //Retrieves, but does not remove, the head (first element) of this list.
+    // Retrieves, but does not remove, the head (first element) of this list.
     @Override
     public E element() throws NoSuchElementException {
         if (this.isEmpty()) {
@@ -75,7 +143,7 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         }
     }
 
-    //Retrieves and removes the head (first element) of this list.
+    // Retrieves and removes the head (first element) of this list.
     @Override
     public E poll() {
         if (this.isEmpty()) {
@@ -88,7 +156,7 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         }
     }
 
-    //Retrieves and removes the head (first element) of this list.
+    // Retrieves and removes the head (first element) of this list.
     @Override
     public E remove() throws NoSuchElementException {
         if (this.isEmpty()) {
@@ -101,7 +169,7 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         }
     }
 
-    //Adds the specified element as the tail (last element) of this list.
+    // Adds the specified element as the tail (last element) of this list.
     @Override
     public boolean offer(E e) {
         if (this.isEmpty()) {
@@ -114,6 +182,82 @@ public class LinkedList<E> extends AbstractCollection<E> implements Queue<E> {
         }
 
         return true;
+    }
+
+    // Inserts the specified element at the front of this list.
+    @Override
+    public boolean offerFirst(E e) {
+        if (this.isEmpty()) {
+            this.head = new Node<>(e);
+            this.tail = this.head;
+        } else {
+            this.head.prev = new Node<>(e);
+            this.head.prev.next = this.head;
+            this.head = this.head.prev;
+        }
+
+        return true;
+    }
+
+    // Inserts the specified element at the end of this list.
+    @Override
+    public boolean offerLast(E e) {
+        if (this.isEmpty()) {
+            this.head = new Node<>(e);
+            this.tail = this.head;
+        } else {
+            this.tail.next = new Node<>(e);
+            this.tail.next.prev = this.tail;
+            this.tail = this.tail.next;
+        }
+
+        return true;
+    }
+
+    // Retrieves, but does not remove, the first element of this list, or returns null if this list is empty.
+    @Override
+    public E peekFirst() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            return this.head.val;
+        }
+    }
+
+    // Retrieves, but does not remove, the last element of this list, or returns null if this list is empty.
+    @Override
+    public E peekLast() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            return this.tail.val;
+        }
+    }
+
+    // Retrieves and removes the first element of this list, or returns null if this list is empty.
+    @Override
+    public E pollFirst() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            E returnElement = this.head.val;
+            this.head = this.head.next;
+            this.head.prev = null;
+            return returnElement;
+        }
+    }
+
+    // Retrieves, but does not remove, the first element of this list, or returns null if this list is empty.
+    @Override
+    public E pollLast() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            E returnElement = this.tail.val;
+            this.tail = this.tail.prev;
+            this.tail.next = null;
+            return returnElement;
+        }
     }
 
     @Override
--- Main.java
+++ Main.java
@@ -1,16 +1,42 @@
 public class Main {
 
     public static void main(String[] args) {
-        Queue<Integer> queue = new LinkedList<>();
+        // Queue<Integer> queue = new LinkedList<>();
 
-        queue.add(0);
-        queue.add(1);
-        queue.add(2);
-        queue.add(3);
+        // queue.add(0);
+        // queue.add(1);
+        // queue.add(2);
+        // queue.add(3);
 
-        System.out.println(queue.remove());
-        System.out.println(queue.element());
+        // System.out.println(queue.remove());
+        // System.out.println(queue.element());
 
-        System.out.println(queue);
+        // System.out.println(queue);
+
+        Deque<Integer> deque = new LinkedList<>();
+
+        deque.add(0);
+        deque.addFirst(-1);
+        deque.addLast(1);
+
+        deque.offerFirst(-2);
+        deque.offerLast(2);
+
+        deque.addFirst(-3);
+        deque.addLast(3);
+
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
+
+        System.out.println(deque.pollFirst());
+        System.out.println(deque.pollLast());
+
+        System.out.println(deque.getFirst());
+        System.out.println(deque.getLast());
+
+        System.out.println(deque.peekFirst());
+        System.out.println(deque.peekLast());
+
+        System.out.println(deque);
     }
 }
